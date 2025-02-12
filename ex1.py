9e6237ce8fbdda98d6dd7978eb328733b9f8af54
Hadi-mhmd <hadiy.mohamadd@gmail.com>
2025-02-12 22:09:51 +0200

    condition done <> ex1

--- ex1.py
+++ ex1.py
@@ -1,2 +1,8 @@
-age = int(input("enter your age"))
+age = int(input("enter your age "))
 
+if age<=5:
+    print("the ticket is free for children")
+elif age<15:
+    print("the ticket price is 8$")   
+else:
+    print('the ticket price is 20$')     
