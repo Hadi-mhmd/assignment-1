b60b711d0b59fb1d0c5a6c93ed658c367cc342e3
Hadi-mhmd <hadiy.mohamadd@gmail.com>
2025-02-12 22:24:21 +0200

    conditon done  <> ex3

--- ex3.py
+++ ex3.py
@@ -1,2 +1,7 @@
-username = input("enter your username ")
-password = input("enter your password ")
+username = input("enter your username: ")
+password = input("enter your password: ")
+
+if username=="admin" and password=="1234":
+    print(f"Access granted for {username}")
+else:
+    print(f"Access denied for {username}")    
